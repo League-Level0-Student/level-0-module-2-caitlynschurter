6f238e98d24ca14c0d8fce58b5e0a119084e9952
Schurter.Caitlyn <Schurter.Caitlyn@SEMS-259-33.SEMS259.LAB>
2018-01-24 16:23:13 -0800

    Animal noises... that's basically it.

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -18,9 +18,39 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
+	
+	boolean quit=false;
+	while(quit==false) {
+	
+	String Animal = JOptionPane.showInputDialog("Which animal would you like to choose? \n Cow, Duck, Cat, Llama or Dog?"
+			+ "\n Please capitalize your first letter \n Type in Quit to stopCat");
+	
+	if (Animal.equals ("Cow")) {
+		playMoo();
+	}
+	
 
+	if (Animal.equals ("Duck")) {
+		playQuack();
+	}
+	
+	if (Animal.equals ("Dog")) {
+		playWoof();
+	}
+	
+	if (Animal.equals ("Cat")) {
+		playMeow();
+	}
+	
+	if (Animal.equals ("Llama")) {
+		playllama();
+	}
+	
+	if (Animal.equals ("Quit")) {
+		quit=true;
+	}
 	/* 2. Make it so that the user can keep entering new animals. */
-}
+}}
 
 void playMoo() {
 	playNoise(mooFile);
@@ -32,6 +62,14 @@ void playQuack() {
 
 void playWoof() {
 	playNoise(woofFile);
+}
+
+void playMeow()	{
+	playNoise(meowFile);
+}
+
+void playllama()	{
+	playNoise(llamaFile);
 }
 
 String quackFile = "quack.wav";
